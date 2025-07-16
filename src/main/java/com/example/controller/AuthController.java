1464dd3109ff8952fd6d22358e29e28f04c086e5
Veeramani A <veeramaniamk@gmail.com>
2025-07-16 16:02:27 +0530

    authcontroller uneccesary blocks are removed

--- src/main/java/com/example/controller/AuthController.java
+++ src/main/java/com/example/controller/AuthController.java
@@ -1,28 +1,11 @@
 package com.example.controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
-// import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.exception.AuthException;
-
-// import com.example.uril.Constance;
-
 @RestController
 public class AuthController {
-
-    @GetMapping("/public")
-    public String publicEndpoint() throws AuthException {
-        throw new AuthException("For Testing");
-        // return "This is a public page";
-    }
-
-    @GetMapping("/user")
-    public String userEndpoint() {
-        return "Hello, authenticated USER!";
-    }
 
     @GetMapping("/dashboard")
     public String dashboardEndpoint() {
@@ -35,11 +18,7 @@ public class AuthController {
                 .header("Location", "/dashboard")
                 .build();
     }
-
-    @GetMapping("/")
-    public String home(Model model) {
-        model.addAttribute("message", "Welcome to the Home Page!");
-        return "${appName}"; // Return view name (home.html)
-    }
     
 }
+
+
