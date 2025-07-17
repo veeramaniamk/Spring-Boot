399421f9e7c56b3881016e2298690efab752b7fb
Veeramani A <veeramaniamk@gmail.com>
2025-07-17 15:06:06 +0530

    generated value added for primary key

--- src/main/java/com/example/entity/Users.java
+++ src/main/java/com/example/entity/Users.java
@@ -1,6 +1,8 @@
 package com.example.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
@@ -12,6 +14,7 @@ public class Users {
     }
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
     private String email;
