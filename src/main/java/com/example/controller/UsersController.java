4511da18f1b79eb771856d7af1458a96bc9c0ec1
Veeramani A <veeramaniamk@gmail.com>
2025-07-18 14:02:14 +0530

    usercontroller created

---
+++ src/main/java/com/example/controller/UsersController.java
@@ -0,0 +1,17 @@
+package com.example.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+public class UsersController {
+
+    @PostMapping
+    private String createUser() {
+
+        return "";
+    }
+    
+}
