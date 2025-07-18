6906f96d5ed1453c7e72aba6bc5fb0678f616bdf
Veeramani A <veeramaniamk@gmail.com>
2025-07-18 14:47:18 +0530

    user create api completed

--- src/main/java/com/example/controller/UsersController.java
+++ src/main/java/com/example/controller/UsersController.java
@@ -1,17 +1,30 @@
 package com.example.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.service.UserService;
 
 @RestController
 @RequestMapping("/api/users")
 public class UsersController {
 
-    @PostMapping
-    private String createUser() {
+    @Autowired
+    UserService userService;
 
-        return "";
+    @PostMapping
+    private String createUser(@RequestParam String name,
+                         @RequestParam String email) {
+
+        if(userService.createUser(name, email)==null) {
+            return "User Creation Failed";
+        }
+
+        return "welcome " + name;
     }
     
 }
---
+++ src/main/java/com/example/service/UserService.java
@@ -0,0 +1,23 @@
+package com.example.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.entity.Users;
+import com.example.repository.UsersRepository;
+
+@Service
+public class UserService {
+    
+    @Autowired
+    UsersRepository usersRepository;
+
+    public Users createUser(String name, String email) {
+        return usersRepository.save(new Users(name, email));
+    }
+
+    // public Users deleteUser(long userId) {
+    //     return usersRepository.deleteById(userId);
+    // }
+
+}
